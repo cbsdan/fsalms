71ca544645509c4b9ab701af0abfba66c119a080
Daniel Cabasa <cabasadaniel1@gmail.com>
2023-11-05 20:37:44 +0800

    Seperate php file for admin ui and user ui

---
+++ administrator-ui.php
@@ -0,0 +1,32 @@
+<?php
+    include_once('./includes/header.php')
+?>
+
+<div id="left-side-navbar">
+    <?php
+        include_once('./administrator/left-side-navbar.php');
+    ?>
+</div>
+
+<main id="main" class="admin"> <!--class should have admin or user-->
+    <div class="section admin"> <!--class should have admin or user-->
+        <?php
+            include_once('./administrator/dashboard-overview.php');
+        ?> 
+    </div>
+</main>
+<div class="screen-toggle"><img src="./img/full-screen.svg" id="screen-logo"> <span>Make Full Screen</span></div>
+<a href="#main" class="scroll-top"><img src="./img/up-icon-light.svg" id="scroll-top-logo"></a>
+<?php
+    include_once('./includes/footer.php')
+?>
+<script>
+    let headerEl = document.querySelector('header')
+    let footerEl = document.querySelector('footer')
+
+    headerEl.classList.remove('user');
+    footerEl.classList.remove('user');
+
+    footerEl.classList.add('admin');
+    footerEl.classList.add('admin');
+</script>
---
+++ user-ui.php
@@ -0,0 +1,32 @@
+<?php
+    include_once('./includes/header.php')
+?>
+
+<main id="main" class="user"> 
+    <div class="info-nav-container">
+        <?php
+            include_once('./user/user_info-nav.php');
+        ?>
+    </div>
+    <div class="section user">
+        <?php
+            include_once('./user/info_section.php');
+        ?> 
+
+    </div>
+</main>
+<div class="screen-toggle"><img src="./img/full-screen.svg" id="screen-logo"> <span>Make Full Screen</span></div>
+<a href="#main" class="scroll-top"><img src="./img/up-icon-light.svg" id="scroll-top-logo"></a>
+<?php
+    include_once('./includes/footer.php')
+?>
+<script>
+    let headerEl = document.querySelector('header')
+    let footerEl = document.querySelector('footer')
+
+    headerEl.classList.remove('admin');
+    footerEl.classList.remove('admin');
+
+    footerEl.classList.add('user');
+    footerEl.classList.add('user');
+</script>
