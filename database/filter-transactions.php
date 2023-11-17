b35c9483460f7cfa491c4e3987f74bfc3a8df1bd
Daniel Cabasa <cabasadaniel1@gmail.com>
2023-11-17 19:27:45 +0800

    Add query to fetch transactions in edit transaction page

--- database/filter-transactions.php
+++ database/filter-transactions.php
@@ -1,6 +1,8 @@
 <?php 
     //use to filter record, if all, claim or unclaim
-    if (isset($_POST['transaction-options'])) {
+    
+    //This is on member transaction page
+    if (isset($_POST['transaction-options']) && $_POST['activeSection'] != './administrator/administrator-edit-transaction.php') {
         try {
             session_start();
             $database_path = '../database/config.php';
@@ -11,7 +13,7 @@
             } else if (file_exists($database_path_index)){
                 include($database_path_index);
             }
-    
+            
             $filterRecord = $_POST['transaction-options'];  
             $_SESSION['activity'] = $filterRecord;
 
@@ -19,10 +21,35 @@
             $_SESSION['message'] = "Failed to change status filter. Error: $e";
             $_SESSION['messageBg'] = 'red';
         }
+        $_SESSION['section'] = './administrator/member-transactions.php';
+        $_SESSION['activeNavId'] = 'm-transactions';
     } 
     
-    $_SESSION['section'] = './administrator/member-transactions.php';
-    $_SESSION['activeNavId'] = 'm-transactions';
+    //This is on edit transactions page
+    if (isset($_POST['transaction-options']) && $_POST['activeSection'] == './administrator/administrator-edit-transaction.php') {
+        try {
+            session_start();
+            $database_path = '../database/config.php';
+            $database_path_index = './database/config.php';
+    
+            if (file_exists($database_path)) {
+                include($database_path);
+            } else if (file_exists($database_path_index)){
+                include($database_path_index);
+            }
+            
+            $filterRecord = $_POST['transaction-options'];  
+            $_SESSION['activity'] = $filterRecord;
+
+        } catch (Exception $e) {
+            $_SESSION['message'] = "Failed to change status filter. Error: $e";
+            $_SESSION['messageBg'] = 'red';
+        }
+
+        $_SESSION['section'] = './administrator/administrator-edit-transaction.php';
+        $_SESSION['activeNavId'] = 'a-editTransaction';
+    } 
+
     header('Location: ../administrator-ui.php');
     exit();
 ?>
