b09f80edff24cce999e7fa5f7b4d298a82ffd1e3
Daniel Cabasa <cabasadaniel1@gmail.com>
2023-11-18 18:19:36 +0800

    Add feature to filter transaction in user ui in transaction nav

--- database/filter-transactions.php
+++ database/filter-transactions.php
@@ -1,19 +1,19 @@
 <?php 
+    session_start();
+    $database_path = '../database/config.php';
+    $database_path_index = './database/config.php';
+
+    if (file_exists($database_path)) {
+        include($database_path);
+    } else if (file_exists($database_path_index)){
+        include($database_path_index);
+    }
+
     //use to filter record, if all, claim or unclaim
     
     //This is on member transaction page
-    if (isset($_POST['transaction-options']) && $_POST['activeSection'] != './administrator/administrator-edit-transaction.php') {
+    if (isset($_POST['transaction-options']) && $_POST['activeSection'] != './administrator/administrator-edit-transaction.php' && $_POST['activeSection'] != './user/transactions_section.php') {
         try {
-            session_start();
-            $database_path = '../database/config.php';
-            $database_path_index = './database/config.php';
-    
-            if (file_exists($database_path)) {
-                include($database_path);
-            } else if (file_exists($database_path_index)){
-                include($database_path_index);
-            }
-            
             $filterRecord = $_POST['transaction-options'];  
             $_SESSION['activity'] = $filterRecord;
 
@@ -23,21 +23,14 @@
         }
         $_SESSION['section'] = './administrator/member-transactions.php';
         $_SESSION['activeNavId'] = 'm-transactions';
+        
+        header('Location: ../administrator-ui.php');
+        exit();
     } 
     
     //This is on edit transactions page
     if (isset($_POST['transaction-options']) && $_POST['activeSection'] == './administrator/administrator-edit-transaction.php') {
-        try {
-            session_start();
-            $database_path = '../database/config.php';
-            $database_path_index = './database/config.php';
-    
-            if (file_exists($database_path)) {
-                include($database_path);
-            } else if (file_exists($database_path_index)){
-                include($database_path_index);
-            }
-            
+        try {  
             $filterRecord = $_POST['transaction-options'];  
             $_SESSION['activity'] = $filterRecord;
 
@@ -48,8 +41,28 @@
 
         $_SESSION['section'] = './administrator/administrator-edit-transaction.php';
         $_SESSION['activeNavId'] = 'a-editTransaction';
+        
+        header('Location: ../administrator-ui.php');
+        exit();
     } 
 
-    header('Location: ../administrator-ui.php');
-    exit();
+
+    if (isset($_POST['transaction-options']) && $_POST['activeSection'] == './user/transactions_section.php') {
+        try {  
+            $filterRecord = $_POST['transaction-options'];  
+            $_SESSION['activity'] = $filterRecord;
+
+        } catch (Exception $e) {
+            $_SESSION['message'] = "Failed to change status filter. Error: $e";
+            $_SESSION['messageBg'] = 'red';
+        }
+
+        $_SESSION['section'] = $_POST['activeSection'];
+        $_SESSION['activeNavId'] = 'transaction-nav';
+
+        header('Location: ../user-ui.php');
+        exit();
+    }
+    
+    
 ?>
